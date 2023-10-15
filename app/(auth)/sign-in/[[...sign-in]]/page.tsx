491065431c115db97baacc730c6b5d099e86c9db
Ricky Firmansyah <ricky.firmansyah@dansmultipro.com>
2023-10-15 20:20:46 +0700

    fix error

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,20 +1,5 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  const stylingPage = {
-    container: {
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      minHeight: '100vh',
-    }
-  };
-
-  return (
-    <div style={stylingPage.container}>
-      <div>
-        <SignIn />
-      </div>
-    </div>
-  );
+  return <SignIn />;
 }
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,20 +1,5 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  const stylingPage = {
-    container: {
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      minHeight: '100vh',
-    }
-  };
-
-  return (
-    <div style={stylingPage.container}>
-      <div>
-        <SignUp />
-      </div>
-    </div>
-  );
+  return <SignUp />;
 }
